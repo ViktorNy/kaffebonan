1c6c4b8c8479212b3af7e9e94ef6f4d995cb55dd
Rasmus Nygren <87rasnyg@skola.boras.se>
2021-09-08 13:37:00 +0200

    added conditional rendering

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import { makeStyles, createStyles } from '@material-ui/styles';
+import { Theme } from 'pretty-format';
+import React, { useContext } from 'react';
 import { CheckOutTable } from '../components/CheckOutTable';
 import OrderForm from '../components/OrderForm';
+import { CartContext } from '../context/CartContext';
 
 export const CheckoutPage = () => {
-    return (
-        <div>
-            <CheckOutTable/>
-            <OrderForm/>
-        </div>
-    )   
+
+    const classes = useStyles();
+
+    const { shoppingCart } = useContext(CartContext);
+
+    if (shoppingCart.length > 0) {
+        return (
+            <div>
+                <CheckOutTable />
+                <OrderForm />
+            </div>)
+    }
+    else {
+        return (
+            < div className={classes.MainStyle}> Nothing in the basket </div>
+        )
+    }
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        MainStyle: {
+            marginTop: '5rem'
+        }
+    }));
