c1764a729e4877775d71f24c9ed19af9e7743ad0
Viktor Nyström <95viknys@skola.boras.se>
2021-09-08 15:51:38 +0200

    Testing grid styling for checkout page

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent, useContext, useState } from "react";
 import TextField from "@material-ui/core/TextField";
-import { Button, createStyles, makeStyles, Theme } from "@material-ui/core";
+import { Button, createStyles, Grid, makeStyles, Theme } from "@material-ui/core";
 import { Redirect } from 'react-router-dom';
 import { CartContext } from "../context/CartContext";
 
@@ -31,84 +31,93 @@ const OrderForm = () => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <div className={classes.outerFormMargin}>
-                <TextField
-                    className={classes.formmargin}
-                    style={{ width: "95%" }}
-                    label="E-mail"
-                    variant="filled"
-                    required
-                    type="email"
-                    value={formValues.email}
-                    onChange={e => {
-                        setformValues({ ...formValues, email: e.target.value })
-                    }}
-                />
-            </div>
-            <div>
-                <TextField
-                    className={classes.formmargin}
-                    style={{ width: "45%" }}
-                    label="First Name"
-                    variant="filled"
-                    required
-                    value={formValues.firstName}
-                    onChange={e => {
-                        setformValues({ ...formValues, firstName: e.target.value })
-                    }}
-                />
-                <TextField
-                    className={classes.formmargin}
-                    style={{ width: "45%" }}
-                    label="Last Name"
-                    variant="filled"
-                    required
-                    value={formValues.lastName}
-                    onChange={e => {
-                        setformValues({ ...formValues, lastName: e.target.value })
-                    }}
-                />
-            </div>
-            <div>
-                <TextField
-                    className={classes.formmargin}
-                    label="Address"
-                    variant="filled"
-                    required
-                    value={formValues.address}
-                    onChange={e => {
-                        setformValues({ ...formValues, address: e.target.value })
-                    }}
-                />
-                <TextField
-                    className={classes.formmargin}
-                    label="Zip Code"
-                    variant="filled"
-                    required
-                    value={formValues.zipCode}
-                    inputProps={{ pattern: "[0-9]*", minLength: 5, maxLength: 5 }}
-                    onChange={e => {
-                        setformValues({ ...formValues, zipCode: e.target.value.toString() })
-                    }}
-                />
-                <TextField
-                    className={classes.formmargin}
-                    label="City"
-                    variant="filled"
-                    required
-                    value={formValues.city}
-                    onChange={e => {
-                        setformValues({ ...formValues, city: e.target.value })
-                    }}
-                />
-            </div>
-            <div className={classes.center + ' ' + classes.contentMargin}>
-                <Button variant="contained" color="primary" type="submit">
-                    Submit
-                </Button>
-                {doRedirect && < Redirect to="/confirmation" />}
-            </div>
-
+            <Grid container spacing={1} className={classes.gridClass}>
+                <Grid item xs={12}>
+                    <div className={classes.outerFormMargin}>
+                        <TextField
+                            className={classes.formmargin}
+                            style={{ width: "95%" }}
+                            label="E-mail"
+                            variant="filled"
+                            required
+                            type="email"
+                            value={formValues.email}
+                            onChange={e => {
+                                setformValues({ ...formValues, email: e.target.value })
+                            }}
+                        />
+                    </div>
+                </Grid>
+                <Grid item xs={8} sm={12}>
+                    <div>
+                        <TextField
+                            className={classes.formmargin}
+                            style={{ width: "45%" }}
+                            label="First Name"
+                            variant="filled"
+                            required
+                            value={formValues.firstName}
+                            onChange={e => {
+                                setformValues({ ...formValues, firstName: e.target.value })
+                            }}
+                        />
+                        <TextField
+                            className={classes.formmargin}
+                            style={{ width: "45%" }}
+                            label="Last Name"
+                            variant="filled"
+                            required
+                            value={formValues.lastName}
+                            onChange={e => {
+                                setformValues({ ...formValues, lastName: e.target.value })
+                            }}
+                        />
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <div>
+                        <TextField
+                            className={classes.formmargin}
+                            label="Address"
+                            variant="filled"
+                            required
+                            value={formValues.address}
+                            onChange={e => {
+                                setformValues({ ...formValues, address: e.target.value })
+                            }}
+                        />
+                        <TextField
+                            className={classes.formmargin}
+                            label="Zip Code"
+                            variant="filled"
+                            required
+                            value={formValues.zipCode}
+                            inputProps={{ pattern: "[0-9]*", minLength: 5, maxLength: 5 }}
+                            onChange={e => {
+                                setformValues({ ...formValues, zipCode: e.target.value.toString() })
+                            }}
+                        />
+                        <TextField
+                            className={classes.formmargin}
+                            label="City"
+                            variant="filled"
+                            required
+                            value={formValues.city}
+                            onChange={e => {
+                                setformValues({ ...formValues, city: e.target.value })
+                            }}
+                        />
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <div className={classes.center + ' ' + classes.contentMargin}>
+                        <Button variant="contained" color="primary" type="submit">
+                            Submit
+                        </Button>
+                        {doRedirect && < Redirect to="/confirmation" />}
+                    </div>
+                </Grid>
+            </Grid>
         </form>
     );
 };
@@ -128,6 +137,8 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         outerFormMargin: {
             marginTop: "3rem"
+        }, gridClass: {
+            display: 'inline-grid'
         }
     }));
 
