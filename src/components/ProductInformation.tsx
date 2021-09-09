a75d39d29bcc2c5abc02237cc43ce38a55e92c72
Viktor Nyström <95viknys@skola.boras.se>
2021-09-09 13:32:12 +0200

    Notifys user when adding product from detail page

--- src/components/ProductInformation.tsx
+++ src/components/ProductInformation.tsx
@@ -1,15 +1,34 @@
 import { Button, createStyles, makeStyles, Theme } from "@material-ui/core"
 import { Product } from "../data"
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 
 interface Props {
     product: Product;
 }
 
+function Alert(props: AlertProps) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 export const ProductInformation = ({ product }: Props) => {
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+        addToCart(product);
+    };
+
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
 
     const { addToCart } = useContext(CartContext);
 
@@ -18,11 +37,14 @@ export const ProductInformation = ({ product }: Props) => {
             <h1>{product?.name}</h1>
             <div className={classes.flex + ' ' + classes.spaceBetween}>
                 <h2>{product.price} kr</h2>
-                <Button color="inherit" onClick={() => {
-                    addToCart(product)
-                }}>
+                <Button color="inherit" onClick={handleClick}>
                     <AddShoppingCartIcon />
                 </Button>
+                <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity="success">
+                        {product.name} added to cart
+                    </Alert>
+                </Snackbar>
             </div>
             <p>{product.info}</p>
         </div>
