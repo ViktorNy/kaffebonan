d2d11539ed87f5680d4347e422f527f480ab8da0
Viktor Nyström <95viknys@skola.boras.se>
2021-09-17 11:32:13 +0200

    Grammatic fix

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
 
     return (
         <div className={classes.mainStyle}>
-            <h1 className={`${classes.contentMargin} ${classes.fontStyle}`}>404 - Kaffe ej funnen</h1>
+            <h1 className={`${classes.contentMargin} ${classes.fontStyle}`}>404 - Kaffe ej hittat</h1>
             <Link to='/' className={classes.textdecoration}>
                 <Button className={`${classes.contentMargin} ${classes.buttonMargin}`} variant='contained' color='primary' type='submit'>
                     Till Startsidan
